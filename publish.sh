523e96f9e59912f457220d5cf1c425065d238135
Chris Wheeler <cwheeler@zipcar.com>
2016-06-23 16:51:16 -0400

    fixed

--- publish.sh
+++ publish.sh
@@ -2,4 +2,5 @@ export LIBVER=$1
 python setup.py egg_info
 python setup.py bdist_wheel
 twine register dist/filtered_intervaltree-$LIBVER-py3-none-any.whl
+twine upload dist/filtered_intervaltree-$LIBVER-py3-none-any.whl
 echo $LIBVER > .library-version
